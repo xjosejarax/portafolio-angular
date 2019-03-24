0813e1f6bf17aa478ee142f22b7226308c3bdfb9
Jose Jara <jose-jara@outlook.com>
2019-03-24 10:48:25 -0500

    final parte 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,17 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPage = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
-    console.log('Servicio cargado');
+   this.cargarInfo();
+   this.cargarEquipo();
+  }
 
+  cargarInfo(){
     // Leer JSON
     this.http.get('assets/data/data-page.json')
     .subscribe( (res: InfoPage) =>{
       this.cargada = true;
       this.info = res;
+    });
+
+  }
+
+  cargarEquipo(){
+    // Leer Firebase
+    this.http.get('https://angular-html-e3240.firebaseio.com/equipo.json')
+    .subscribe( (res: any[]) =>{
+      this.cargada = true;
+      this.equipo = res;
       console.log( res );
-    })
+    });
 
   }
 }
